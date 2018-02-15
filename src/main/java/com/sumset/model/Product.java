ab2305f3829f05e0389ca220c17a9d7d4b863380
Sebastian Rojas Quintero <srojas@sumset.com>
2018-02-15 15:47:34 -0500

    Se agrega persistencia al aplicativo. Se crean los repositorios(DAOS) y una configuración para que las pruebas unitarias inyecten los daos. Solo funciona en pruebas, aún no en el aplicativo

--- src/main/java/com/sumset/model/Product.java
+++ src/main/java/com/sumset/model/Product.java
@@ -2,13 +2,37 @@ package com.sumset.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products") 
 public class Product implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
     private String description;
     private Double price;
     
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    } 
+
     public String getDescription() {
         return description;
     }
